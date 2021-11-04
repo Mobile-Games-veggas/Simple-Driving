c78fad11f06fc67df36ab70012c0d6330b13ef4a
Stanislav Goncharov <stas76644@icloud.com>
2021-11-04 14:32:45 +0300

    Добавил Play button, High Score, сохранение счета при закрытии 29 Main menu

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -8,6 +8,8 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplayer;
 
+    public const string HighScoreKey = "HightScore";
+
     private float score;
 
     // Update is called once per frame
@@ -16,5 +18,15 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMultiplayer;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+    
+    private void OnDestroy() 
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey,0);
+
+        if(score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey,Mathf.FloorToInt(score));
+        }
     }
 }
